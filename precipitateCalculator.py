cbfbf5e1ecede642e5e5845562260b743cc4d407
Chelsea Chen <c.chen13@share.epsb.ca>
2020-05-30 00:05:04 -0600

    precipitate calculator

--- precipitateCalculator.py
+++ precipitateCalculator.py
@@ -4,7 +4,7 @@ author: Chelsea Chen
 date created: 2020-05-27
 """
 # --- Variables/Arrays --- #
-ionName = (("Li",1), ("Mg",2), ("Ca",2), ("Sr",2),("Ba",2),("Fe",2),("Hg",2), ("Pb",2), ("F",1), ("Cl",1), ("Br",1), ("I",1), ("Cu",1), ("Ag",1), ("Tl",1), ("SO4",2), ("Ra",2), ("NH4",1), ("NO3",1), ("ClO4",1), ("CH3COO",1), ("RbClO4",1), ("CsClO4",1), ("AgCH3COO",1), ("Hg2(CH3COO)2",1),("H",1),("Na",1), ("K",1), ("Rb",1), ("Cs",1), ("Fr",1))
+ionName = (("Li",1), ("Mg",2), ("Ca",2), ("Sr",2),("Ba",2),("Fe",2),("Hg2",2), ("Pb",2), ("F",1), ("Cl",1), ("Br",1), ("I",1), ("Cu",1), ("Ag",1), ("Tl",1), ("SO4",2), ("Ra",2), ("NH4",1), ("NO3",1), ("ClO4",1), ("CH3COO",1), ("RbClO4",1), ("CsClO4",1), ("AgCH3COO",1), ("Hg2(CH3COO)2",1),("H",1),("Na",1), ("K",1), ("Rb",1), ("Cs",1), ("Fr",1))
 ionMass =(6.94, 24.31, 40.08, 87.62, 137.33, 55.85, 401.18, 207.2, 19.0, 35.45,79.90,126.90,63.55,107.87,204.38,96.06,226, 18.039, 62.0049,99.451,59.05, 184.918, 232.36,167.03,519.28, 1.01, 22.99, 39.1, 85.47, 132.91, 223)
 again = True
 # --- Inputs --- #
@@ -83,7 +83,6 @@ while again == True:
     positiveSolution[0] = positiveReacting
     positiveSolution[1] = float(positiveVolume)
     positiveSolution[2] = float(positiveConcentration)
-    print(positiveSolution)
 
 # input values for negative ion name, volume, concentration
     negReacting, negVolume, negConcentration = valuesNeg()
@@ -91,7 +90,6 @@ while again == True:
     negSolution[0] = negReacting
     negSolution[1] = negVolume
     negSolution[2] = negConcentration
-    print(negSolution)
 
 # --- Processing --- #
 # balencing chemical equations
@@ -101,25 +99,25 @@ while again == True:
         if positiveSolution[0] == ionName[i][0]:
             reactIon1.append(ionName[i][0])
             reactIon1.append(ionName[i][1])
-    print(reactIon1)
+
     # negative ion (reactant)
     reactIon2= []
     for i in range(len(ionName)):
         if negSolution[0] == ionName[i][0]:
             reactIon2.append(ionName[i][0])
             reactIon2.append(ionName[i][1])
-    print(reactIon2)
+
     # number of positive ion
     productIon1 = []
     productIon1.append(reactIon1[0]) #switch charges
     productIon1.append(reactIon2[1])
-    print(productIon1)
+
     # number of negative ion
     productIon2 = []
     productIon2.append(reactIon2[0]) #switch charges
     productIon2.append(reactIon1[1])
     productMoles = 1
-    print(productIon2)
+
 
 # find mass of product
     massOfProduct = []
@@ -135,18 +133,13 @@ while again == True:
     mass2 = massOfProduct[3]*reactIon1[1]
     mass = mass1 + mass2
     massOfProduct.append(mass)
-    print(mass)
-    print(massOfProduct)
 
 # find moles from concentration and volume
     molesPosReacting = positiveSolution[1] * positiveSolution[2] # find out how to do with arrays
     positiveSolution.append(molesPosReacting)
-    print(molesPosReacting)
-    print(positiveSolution)
+
     molesNegReacting = negSolution[1] * negSolution[2] # find out how to do with arrays
     negSolution.append(molesNegReacting)
-    print(molesNegReacting)
-    print(negSolution)
 
 #find limiting reagent
     # moles of positive ion needed to fully react moles of negative ion
@@ -156,10 +149,6 @@ while again == True:
     # moles of negative ion needed to fully react moles of positive ion
     molesPosReagent = negSolution[3] * (productIon1[1]/productIon2[1])
     positiveSolution.append(molesPosReagent)
-    print(molesNegReagent)
-    print(molesPosReagent)
-    print(negSolution)
-    print(positiveSolution)
 
 #--- Outputs --- #
     # find limiting reagent if positive solution
@@ -169,7 +158,6 @@ while again == True:
         productMoles = 1
         molarRatio = float(productMoles) / float(productIon2[1])
         mass = float(positiveSolution[3]) * float(molarRatio) * float(massOfProduct[4])  # fix for moles of overall product
-        print(mass)
         print("The mass of " + str(positiveSolution[0]) + str(negSolution[0]) + " " + str(round(mass, 2)) + "grams")
 
     # find limiting reagent if negative solution
@@ -179,10 +167,8 @@ while again == True:
         productMoles = 1
         molarRatio = float(productMoles)/ float(productIon2[1])
         mass = float(negSolution[3]) * float(molarRatio) * float(massOfProduct[4]) # fix for moles of overall product
-        print(mass)
         print("The mass of " + str(positiveSolution[0]) + str(negSolution[0]) + " " + str(round(mass,2)) + "grams")
-        print(positiveSolution)
-        print(negSolution)
+
 
     #play again
     again = calculateAgain()
